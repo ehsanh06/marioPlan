efb6b15819d8a015fb7e22207b9c847dd7862465
Ehsan Hussain <ehsanhussain991@gmail.com>
2018-10-10 14:29:18 +0100

    ProjectList modularized by ProjectSummary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,47 +1,20 @@
 import React from 'react';
 
+import ProjectSummary from './ProjectSummary';
+
 const ProjectList = () => {
     return (
         <div className="project-list section">
 
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-
-                    {/* 
-                        Information will be dynamically obtained from database
-                        Hardcoded until database is setup
-                    */}
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ehsan Hussain</p>
-                    <p className="grey-text">12th October, 2pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-
-                    {/* 
-                        Information will be dynamically obtained from database
-                        Hardcoded until database is setup
-                    */}
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ehsan Hussain</p>
-                    <p className="grey-text">12th October, 2pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-
-                    {/* 
-                        Information will be dynamically obtained from database
-                        Hardcoded until database is setup
-                    */}
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Ehsan Hussain</p>
-                    <p className="grey-text">12th October, 2pm</p>
-                </div>
-            </div>
+        {/* 
+            Once hooked up to the database, we'll loop around the database data
+            and only really need to append one <ProjectSummary />. For now this
+            is just placeholding.
+        */}
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+
+                {/* 
+                        Information will be dynamically obtained from database
+                        Hardcoded until database is setup
+                */}
+                <span className="card-title">Project Title</span>
+                <p>Posted by Ehsan Hussain</p>
+                <p className="grey-text">12th October, 2pm</p>
+            </div>
+        </div>
+    )
+};
+
+export default ProjectSummary;
