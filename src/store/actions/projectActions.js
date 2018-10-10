224d7f43c29c48d80652657a509fe888630601ae
Ehsan Hussain <ehsanhussain991@gmail.com>
2018-10-10 17:14:23 +0100

    adding data to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,21 @@
 export const createProject = project => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
-        // Make some Async call to db
-        dispatch({ type: 'CREATE_PROJECT', project });
+
+        // Adding document to database
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Death',
+            authorLastName: 'Born',
+            authorId: 12354,
+            createdAt: new Date()
+        })
+        .then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        })
+        .catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        })
+        
     }
 };
